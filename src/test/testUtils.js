6e3d58fde260178ae698161c117628285adbdd23
Pramit Barua <pramit.barua@gmail.com>
2020-07-10 08:42:53 +0200

    helper function storeFactory added

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,3 +1,23 @@
+import rootReducer from '../Redux/Reducer/rootReducer';
+import { createStore } from 'redux';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ *  globals: rootReducer, middlewares.
+ * @function storeFactory
+ * @param {object} initialState - Initial state for store.
+ * @returns {Store} - Redux store.
+ */
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
+
+/**
+ * Return node(s) with the given data-test attribute.
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} val - Value of data-testid attribute for search.
+ * @returns {ShallowWrapper}
+ */
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-testid="${val}"]`);
 };
